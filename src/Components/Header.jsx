c5dc0e23d75039d1e07e56fe1e170aec11f23baa
vedshree-dhaybar <vedshreedhaybar@gmail.com>
2024-09-16 12:00:05 +0000

    Edited

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,78 +1,50 @@
 function Header() {
   return (
     <>
-      <div className="container">
-        <nav className="navbar">
-          <a href="#">
-            <img
-              src="./assets/images/akatsuki.png"
-              alt="SimpleBlog logo"
-              width="150"
-              className="logo-light"
-            />
-          </a>
+      <header>
+        <div className="container">
+          <nav className="navbar">
+            <a href="#">
+              <img
+                src="./assets/images/akatsuki.png"
+                alt="SimpleBlog logo"
+                width="150"
+                className="logo-light"
+              />
+            </a>
 
-          <div className="btn-group">
-            <button className="theme-btn theme-btn-mobile light">
-              <ion-icon name="moon" className="moon"></ion-icon>
-              <ion-icon name="sunny" className="sun"></ion-icon>
-            </button>
+            <div className="btn-group">
+              <button className="theme-btn theme-btn-mobile light">
+                <ion-icon name="moon" className="moon"></ion-icon>
+                <ion-icon name="sunny" className="sun"></ion-icon>
+              </button>
 
-            <button className="nav-menu-btn">
-              <ion-icon name="menu-outline"></ion-icon>
-            </button>
-          </div>
+              <button className="nav-menu-btn">
+                <ion-icon name="menu-outline"></ion-icon>
+              </button>
+            </div>
 
-          <div className="flex-wrapper">
-            <ul className="desktop-nav">
-              <li>
-                <a href="#" className="nav-link">
-                  Home
-                </a>
-              </li>
-
-              <li>
-                <a href="#" className="nav-link">
-                  Events
-                </a>
-              </li>
-
-              <li>
-                <a href="#" className="nav-link">
-                  About Us
-                </a>
-              </li>
-
-              <li>
-                <a href="#" className="nav-link">
-                  Contact
-                </a>
-              </li>
-            </ul>
-          </div>
-
-          <div className="mobile-nav">
-            <button className="nav-close-btn">
-              <ion-icon name="close-outline"></ion-icon>
-            </button>
-
-            <div className="wrapper">
-              <p className="h3 nav-title">Main Menu</p>
-
-              <ul>
-                <li className="nav-item">
+            <div className="flex-wrapper">
+              <ul className="desktop-nav">
+                <li>
                   <a href="#" className="nav-link">
                     Home
                   </a>
                 </li>
 
-                <li className="nav-item">
+                <li>
                   <a href="#" className="nav-link">
-                    About Me
+                    Events
                   </a>
                 </li>
 
-                <li className="nav-item">
+                <li>
+                  <a href="#" className="nav-link">
+                    About Us
+                  </a>
+                </li>
+
+                <li>
                   <a href="#" className="nav-link">
                     Contact
                   </a>
@@ -80,32 +52,62 @@ function Header() {
               </ul>
             </div>
 
-            <div>
-              <p className="h3 nav-title">Topics</p>
+            <div className="mobile-nav">
+              <button className="nav-close-btn">
+                <ion-icon name="close-outline"></ion-icon>
+              </button>
 
-              <ul>
-                <li className="nav-item">
-                  <a href="#" className="nav-link">
-                    Database
-                  </a>
-                </li>
+              <div className="wrapper">
+                <p className="h3 nav-title">Main Menu</p>
 
-                <li className="nav-item">
-                  <a href="#" className="nav-link">
-                    Accessibility
-                  </a>
-                </li>
+                <ul>
+                  <li className="nav-item">
+                    <a href="#" className="nav-link">
+                      Home
+                    </a>
+                  </li>
 
-                <li className="nav-item">
-                  <a href="#" className="nav-link">
-                    Web Performance
-                  </a>
-                </li>
-              </ul>
+                  <li className="nav-item">
+                    <a href="#" className="nav-link">
+                      About Me
+                    </a>
+                  </li>
+
+                  <li className="nav-item">
+                    <a href="#" className="nav-link">
+                      Contact
+                    </a>
+                  </li>
+                </ul>
+              </div>
+
+              <div>
+                <p className="h3 nav-title">Topics</p>
+
+                <ul>
+                  <li className="nav-item">
+                    <a href="#" className="nav-link">
+                      Database
+                    </a>
+                  </li>
+
+                  <li className="nav-item">
+                    <a href="#" className="nav-link">
+                      Accessibility
+                    </a>
+                  </li>
+
+                  <li className="nav-item">
+                    <a href="#" className="nav-link">
+                      Web Performance
+                    </a>
+                  </li>
+                </ul>
+              </div>
             </div>
-          </div>
-        </nav>
-      </div>
+          </nav>
+        </div>
+      </header>
     </>
   );
 }
