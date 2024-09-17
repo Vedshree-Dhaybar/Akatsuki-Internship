f5381e92b1ca31d0d501b71c179841c37fbb5396
vedshree-dhaybar <vedshreedhaybar@gmail.com>
2024-09-17 15:12:09 +0000

    Added CSS

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import "./styles.css";
-import Header from "./Components/Header.jsx";
-import Hero from "./Components/Hero.jsx";
-import Blogs from "./Components/Blogs.jsx";
-import Aside from "./Components/Aside.jsx";
-import Footer from "./Components/Footer.jsx";
+import "./index.css";
+import Header from "./Components/Header/Header.jsx";
+import Hero from "./Components/Hero/Hero.jsx";
+import Blogs from "./Components/Blogs/Blogs.jsx";
+import Aside from "./Components/Aside/Aside.jsx";
+import Footer from "./Components/Footer/Footer.jsx";
 
 function App() {
   return (
